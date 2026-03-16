984a734cdd103637a6e6d1c7613fbe70b6aaa2f3
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-03-16 19:49:21 +0100

    feat: add ExecuteAsync method tests for process exit codes

---
+++ tests/ProcessTests/src/MethodTests/ExecuteAsycnMethodTests.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using AwesomeAssertions;
+using WB.Toolkit;
+
+namespace ProcessTests.MethodTests.ExecuteAsyncMethodTests;
+
+public sealed class TheExecuteAsyncMethod
+{
+    [Test]
+    [Arguments("--version", 0)]
+    [Arguments("--invalid", 1)]
+    public async Task ShouldReturnTheExitCode(string argument, int exitCode)
+    {
+        // Arrange
+        using Process process = new("dotnet", [argument]);
+
+        // Act
+        int actualExitCode = await process.ExecuteAsync().ConfigureAwait(false);
+
+        // Assert
+        actualExitCode.Should().Be(exitCode, because: "the process should exit with the specified code");
+    }
+}
